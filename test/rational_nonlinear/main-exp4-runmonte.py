782e6cceb5be8c6d0776889c171c64fd8a73d113
jordan787878 <j2855001@gmail.com>
2024-07-01 16:41:07 -0600

    test

--- test/rational_nonlinear/main-exp4-runmonte.py
+++ test/rational_nonlinear/main-exp4-runmonte.py
@@ -738,17 +738,15 @@ def plot_p_monte():
 def main():
 
     FLAG_GENERATE_DATA = True
-    N = 1.0
+    N = 2.0
     if(FLAG_GENERATE_DATA):
         for t1 in t1s:
             x_sim, _ = p_sol_monte(t1=t1, linespace_num=100, stat_sample=1)
             p_sim = 0.0*(x_sim.reshape(-1,1))
-            for i in range(1):
+            for i in range(2):
                 print("generate ", t1, " batch ",i)
                 _ , _p_sim = p_sol_monte(t1=t1, linespace_num=100, stat_sample=1000000)
-            p_sim = p_sim + (_p_sim/N)
-            # _x_sim, _p_sim = p_sol_monte(t1=t1, linespace_num=100, stat_sample=1000000000)
-            # p_sim = 0.5*(p_sim + _p_sim)
+                p_sim = p_sim + (_p_sim/N)
             np.save(DATA_FOLDER+"psim_t"+str(t1)+".npy", p_sim)
             np.save(DATA_FOLDER+"xsim.npy", x_sim)
     # Plot generated data
